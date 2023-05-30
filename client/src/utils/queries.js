096cb9e07d61c6e8f7de2cb65d749471b77b008c
dhclarke99 <dhclarke99@gmail.com>
2023-05-30 12:03:17 -0400

    starter code for Home page

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_Polls = gql`
+  query tech {
+    poll {
+      _id
+      body
+    }
+  }
+`;
+
+export const QUERY_MATCHUPS = gql`
+  query matchups($_id: String) {
+    matchups(_id: $_id) {
+      _id
+      tech1
+      tech2
+      true_votes
+      false_votes
+    }
+  }
+`;
