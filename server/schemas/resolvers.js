de659d64dc18a1e6bd8647664e9a7e84e760f0ae
alboy77 <al.boucetta@yahoo.com>
2023-05-30 13:59:11 -0400

    first test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,29 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Poll, Vote } = require('../models');
+const { signToken } = require('../');
+
 const resolvers = {
     Query: {
-
+        users: async () => {
+            return await User.find();
+        },
+        user: async (parent, {username}) => {
+            return await User.findOne({username}).populate('polls');
+        },
+        polls: async () => {
+            return await Poll.find();
+        },
+        poll: async (parent, {pollId}) => {
+            return await Poll.findOne({_id: pollId})
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findOne({_id: context.user._id}).populate('polls');
+            } throw new AuthenticationError('Not Authenticated')
+        },
     },
     Mutation: {
-
+        
     },
 };
 
