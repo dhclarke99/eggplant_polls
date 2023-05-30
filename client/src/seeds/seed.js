1ca40f7ecd598c45b17d9d4b6d5876f559afba5f
CmEinsig <cmneiderer@gmail.com>
2023-05-30 12:53:48 -0400

    changed typo in seed js for file req

--- client/src/seeds/seed.js
+++ client/src/seeds/seed.js
@@ -1,7 +1,7 @@
 const db = require('../config/connection');
 const { polls } = require('../models');
 
-const pollData = require('./techData.json');
+const pollData = require('./pollData.json');
 
 db.eggplantPolls('open', async () => {
   await polls.deleteMany({});
