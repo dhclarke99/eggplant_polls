2f233bc2967a7b4f792f009f2fc374d90996c609
dhclarke99 <dhclarke99@gmail.com>
2023-05-30 13:45:39 -0400

    small change:

--- server/models/poll.js
+++ server/models/poll.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const pollSchema = new Schema({
     title: {
-        type: string,
+        type: String,
         required: true,
     },
     description: {
