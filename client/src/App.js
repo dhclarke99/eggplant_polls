619f94952770760df5bd789ce9f9550faeef7641
dhclarke99 <dhclarke99@gmail.com>
2023-05-31 13:23:54 -0400

    created Header component and Login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import Home from './pages/Home';
 import Farm from './pages/Farm';
 import Login from './pages/Login';
 import Poll from './pages/Poll';
+import Header from './components/header';
+
+
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -15,25 +37,29 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />}
-            />
-            <Route 
-              path="/farm" 
-              element={<Farm />}
-            />
-            <Route 
-              path="/login" 
-              element={<Login />}
-            />
-            <Route 
-              path="*"
-              element={<Home />}
-            />
-          </Routes>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/"
+                element={<Home />}
+              />
+              <Route 
+                path="/farm" 
+                element={<Farm />}
+              />
+              <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/poll" 
+                element={<Poll />}
+              />
+            </Routes>
+          </div>
+          
         </div>
       </Router>
     </ApolloProvider>
