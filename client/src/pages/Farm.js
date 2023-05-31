4a3a88fb507e30e557287998513b9e00c2ce0d19
dhclarke99 <dhclarke99@gmail.com>
2023-05-31 17:57:32 -0400

    added functionality to retrive ID of user that is logged in when you click farm

--- client/src/pages/Farm.js
+++ client/src/pages/Farm.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import AuthService from '../utils/auth';
 
 function Farm() {
- 
+  
+  const handleClick = () => {
+    console.log(AuthService.getProfile())
+    console.log(AuthService.getProfile().data._id)
+   
+  };
   return (
     <div className="card bg-white card-rounded w-50">
       <div className="card-header bg-dark text-center">
         <h1>
           Farm for Eggplants!
         </h1>
-        <button>Farm</button>
+        <button onClick={handleClick}>Farm</button>
       </div>
     </div>
   );
