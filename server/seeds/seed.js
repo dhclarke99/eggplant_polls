06b382e995e72d8a049d2a66aa4162a1ad95c032
dhclarke99 <dhclarke99@gmail.com>
2023-05-31 14:11:09 -0400

    connected login and signup function

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,17 @@
 const db = require('../config/connection');
 const { Poll } = require('../models');
+const { User } = require('../models');
 
 const pollData = require('./pollData.json');
+const userData = require('./userData.json');
 
 db.once('open', async () => {
   await Poll.deleteMany({});
+  await User.deleteMany({});
 
   const polls = await Poll.insertMany(pollData);
+  const users = await User.insertMany(userData);
 
-  console.log('Polls seeded!');
+  console.log('Polls and Users seeded!');
   process.exit(0);
 });
