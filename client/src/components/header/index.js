e75b29abb60d29cc066d2f3119489af0b6921969
alboy77 <al.boucetta@yahoo.com>
2023-06-02 15:06:45 -0400

    heroku deploy again

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -29,7 +29,7 @@ const Header = () => {
               {loggedInUser?.username}'s profile ({loggedInUser?.eggplants || 0} eggplants)
             </Link></ul>
             <ul><Link className="btn btn-lg btn-light m-2" to="/farm">
-              Farm
+              Farm.
             </Link></ul>
             <ul><button className="btn btn-lg btn-light m-2" onClick={logout}>
               Logout
