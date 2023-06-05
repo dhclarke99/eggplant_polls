a21d100021299835fa5b746733e37ebb1b2ca526
alboy77 <al.boucetta@yahoo.com>
2023-06-04 23:20:10 -0400

    idk

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -21,13 +21,13 @@ const { loading, error, data } = useQuery(QUERY_USER_by_id, {
 });
 
 const userData = data?.user;
-console.log(userData)
+
 
   return (
     <header className="bg-primary text-light mb-4 py-3">
       <div class="container-fluid">
-      <img src="./assets/images/eggplantBox.png" alt="Box of Eggplants" id="headerimg"></img>
-        <div class="navbar-brand" id="navbar">EggPlant Polls 
+      <img src="./" alt="Box of Eggplants" id="headerimg"></img>
+        <div class="navbar-brand" id="navbar">Eggplant Polls 
           <p className="m-0" id="wager" style={{ listStyleType: 'none' }}>Wager your Eggplant currency on poll results!</p></div>
       </div>
       <ul>
@@ -38,7 +38,7 @@ console.log(userData)
             </Link></ul>
             <ul><Link className="btn btn-lg btn-info m-2" to="/me">
               {AuthService.getProfile().data.username}'s profile
-              {userData?.username} ({loggedInUser?.eggplants || 0} eggplants)
+              {userData?.username} ({loggedInUser.eggplants || 0} eggplants)
             </Link></ul>
             <li style={{ listStyleType: 'none' }}><Link className="btn btn-lg btn-light m-2" to="/farm">
               Farm
